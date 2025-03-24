23b5adb238decf9fae5119ea5cd95df24f73a4ab
Mohamedelrokh <mohamed.elrokh@devoteam.com>
2025-03-25 01:27:01 +0300

    Update schedule.bash

--- Batch/dbt/green/schedule.bash
+++ Batch/dbt/green/schedule.bash
@@ -4,5 +4,6 @@ Steps to Schedule a Job in dbt Cloud:
 3- create dbt environment for deployment  (e.g., Production).
 4- Create a Job
 5-dbt run --select green_full_trips
+  dbt run --select yellow_full_trips
 6- Schedule the Job(6 hr)
 7- save
