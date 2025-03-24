0e11022aa82b44fd71d4903914f3c7f8b17ba6e0
Mohamedelrokh <mohamed.elrokh@devoteam.com>
2025-03-25 01:07:47 +0300

    Create green_full_trips.sql

---
+++ Batch/dbt/green/green_full_trips.sql
@@ -0,0 +1,26 @@
+WITH aggregated_data AS (
+    SELECT * FROM {{ ref('grouped_trips') }}  -- Reference the aggregation model
+)
+
+SELECT
+    a.*,
+    pu.Zone AS pickup_zone,
+    pu.Borough AS pickup_borough,
+    pu.service_zone AS pickup_service_zone ,
+    pu.latitude AS pickup_latitute ,
+    pu.longitude AS pickup_longitude ,
+    do.Zone AS dropoff_zone,
+    do.Borough AS dropoff_borough,
+    do.service_zone AS dropoff_service_zone,
+    do.latitude AS dropoff_latitute ,
+    do.longitude AS dropoff_longitude ,
+FROM
+    aggregated_data a
+LEFT JOIN
+    {{ ref('taxi_zones_coordinates') }} pu
+    ON a.PULocationID = pu.LocationID
+LEFT JOIN
+    {{ ref('taxi_zones_coordinates') }} do
+    ON a.DOLocationID = do.LocationID
+ORDER BY
+    a.pickup_year, a.pickup_month, a.pickup_day
