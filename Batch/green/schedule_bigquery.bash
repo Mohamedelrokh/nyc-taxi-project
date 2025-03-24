85d6941fa5e314871a798956ab9fbb02596a544a
Mohamedelrokh <mohamed.elrokh@devoteam.com>
2025-03-24 12:25:07 +0300

    Update schedule_bigquery.bash

--- Batch/green/schedule_bigquery.bash
+++ Batch/green/schedule_bigquery.bash
@@ -3,22 +3,21 @@ bq mk --transfer_config \
     --transfer_config_id=my_scheduled_query \
     --data_source=scheduled_query \
     --destination_dataset_id=taxi_data \
-    --display_name="Insert Raw Data to Partitioned Table Every 6 Hours" \
+    --display_name="Insert green Raw Data to Partitioned Table Every 6 Hours" \
     --schedule="every 6 hours" \
     --query="
-        INSERT INTO \`di-malrokh-sandbox-malrokh.taxi_data.partitioned_trips\` (
+        INSERT INTO \`di-malrokh-sandbox-malrokh.taxi_data.green_trips_partitioned\` (
             VendorID, lpep_pickup_datetime, lpep_dropoff_datetime, store_and_fwd_flag, RatecodeID, 
             PULocationID, DOLocationID, passenger_count, trip_distance, fare_amount, extra, mta_tax, 
             tip_amount, tolls_amount, ehail_fee, improvement_surcharge, total_amount, payment_type, 
-            trip_type, congestion_surcharge, __index_level_0__
+            trip_type, congestion_surcharge
         )
         SELECT
             VendorID, lpep_pickup_datetime, lpep_dropoff_datetime, store_and_fwd_flag, RatecodeID, 
             PULocationID, DOLocationID, passenger_count, trip_distance, fare_amount, extra, mta_tax, 
             tip_amount, tolls_amount, ehail_fee, improvement_surcharge, total_amount, payment_type, 
-            trip_type, congestion_surcharge, __index_level_0__
-        FROM \`di-malrokh-sandbox-malrokh.taxi_data.raw_trips\`
+            trip_type, congestion_surcharge
+        FROM \`di-malrokh-sandbox-malrokh.taxi_data.green_trips_raw\`
         WHERE lpep_pickup_datetime >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 6 HOUR)
     " \
-    --notification_pubsub_topic="projects/di-malrokh-sandbox-malrokh/topics/your_topic" \
     --write_disposition=WRITE_APPEND
