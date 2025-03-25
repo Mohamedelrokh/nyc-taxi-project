a276d0ffcb5ffa44bc74d183aeebf4142e987b19
Mohamedelrokh <mohamed.elrokh@devoteam.com>
2025-03-25 05:20:30 +0300

    Create green_dag.py

---
+++ Batch/green/green_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.dataflow import DataflowCreatePythonJobOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 3, 25),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+    "dataflow_job_every_hour",
+    default_args=default_args,
+    description="Run Dataflow job every hour",
+    schedule_interval="0 * * * *",  # Runs at the start of every hour
+    catchup=False,
+)
+
+dataflow_task = DataflowCreatePythonJobOperator(
+    task_id="run_dataflow_job",
+    py_file="gs://green-trips/scripts/process_data7.py",  # Ensure script is in GCS
+    job_name="process",
+    options={
+        "runner": "DataflowRunner",
+        "project": "di-malrokh-sandbox-malrokh",
+        "region": "us-central1",
+        "staging_location": "gs://green-trips/staging/",
+        "temp_location": "gs://green-trips/temp/",
+    },
+    location="us-central1",
+    dag=dag,
+)
